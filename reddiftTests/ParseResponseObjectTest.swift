a095f09a883cc0966a0634f7f8d7ffb084e58a61
sonson <yoshida.yuichi@gmail.com>
2015-04-22 23:31:46 +0900

    Added test code for message and subreddit.

--- reddiftTests/ParseResponseObjectTest.swift
+++ reddiftTests/ParseResponseObjectTest.swift
@@ -67,55 +67,61 @@ class ParseResponseObjectTest: XCTestCase {
             XCTFail("JSON error")
         }
     }
-
-    //    func testLinkList() {
-    //        if let path = NSBundle(forClass: self.classForCoder).pathForResource("links", ofType: "json") {
-    //            if let data = NSData(contentsOfFile: path) {
-    //                if let json:AnyObject = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.allZeros, error: nil) {
-    //					if let listing = Parser.parseJSON(json, depth:0) as? Listing {
-    //						for link in listing.children {
-    //							if let link = link as? Link {
-    //								println(link.toString())
-    //							}
-    //						}
-    //					}
-    //                }
-    //            }
-    //        }
-    //    }
-    //
-    //    func testComment() {
-    //        if let path = NSBundle(forClass: self.classForCoder).pathForResource("comments01", ofType: "json") {
-    //            if let data = NSData(contentsOfFile: path) {
-    //                if let json:AnyObject = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.allZeros, error: nil) {
-    //                    var obj:AnyObject? = Parser.parseJSON(json, depth:0)
-    //                    if let array = obj as? [AnyObject] {
-    //                        for element in array {
-    //                            if let listing = element as? Listing {
-    //                                println(listing.toString())
-    //                            }
-    //                        }
-    //                    }
-    //                }
-    //            }
-    //        }
-    //    }
-    //
     
-    
-    func testSubreddits() {
-        if let path = NSBundle(forClass: self.classForCoder).pathForResource("subreddit", ofType: "json") {
-            if let data = NSData(contentsOfFile: path) {
-                if let json:AnyObject = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.allZeros, error: nil) {
-                    if let listing = Parser.parseJSON(json, depth:0) as? Listing {
-                        for subreddit in listing.children {
-                            if let subreddit = subreddit as? Subreddit {
-                                println(subreddit.toString())
-                            }
-                        }
-                    }
+    func testResponse_link() {
+        let json:AnyObject? = self.jsonFromFileName("links.json")
+        if let json:AnyObject = json {
+            if let listing = Parser.parseJSON(json, depth:0) as? Listing {
+                XCTAssertEqual(listing.children.count, 26, "Check 2 Listing objects")
+                for child in listing.children {
+                    XCTAssert((child is Link), "Check class of children.")
                 }
             }
+            else {
+                XCTFail("JSON error")
+            }
+        }
+        else {
+            XCTFail("JSON error")
+        }
+    }
+    
+    func testResponse_message() {
+        let json:AnyObject? = self.jsonFromFileName("message.json")
+        if let json:AnyObject = json {
+            if let listing = Parser.parseJSON(json, depth:0) as? Listing {
+                XCTAssertEqual(listing.children.count, 4, "Check 2 Listing objects")
+                if listing.children.count == 4 {
+                    XCTAssert((listing.children[0] is Comment), "Check class of children.")
+                    XCTAssert((listing.children[1] is Comment), "Check class of children.")
+                    XCTAssert((listing.children[2] is Comment), "Check class of children.")
+                    XCTAssert((listing.children[3] is Message), "Check class of children.")
+                }
+            }
+            else {
+                XCTFail("JSON error")
+            }
+        }
+        else {
+            XCTFail("JSON error")
+        }
+    }
+
+    func testResponse_subreddit() {
+        let json:AnyObject? = self.jsonFromFileName("subreddit.json")
+        if let json:AnyObject = json {
+            if let listing = Parser.parseJSON(json, depth:0) as? Listing {
+                XCTAssertEqual(listing.children.count, 5, "Check 2 Listing objects")
+                for child in listing.children {
+                    XCTAssert((child is Subreddit), "Check class of children.")
+                }
+            }
+            else {
+                XCTFail("JSON error")
+            }
+        }
+        else {
+            XCTFail("JSON error")
         }
     }
     
