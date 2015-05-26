b9c0c166ae05768199c4dc8a024397761087f4e4
sonson <yoshida.yuichi@gmail.com>
2015-05-26 15:46:31 +0900

    Try to use Thing type....

--- reddift/Utility/Parser.swift
+++ reddift/Utility/Parser.swift
@@ -17,30 +17,30 @@ class Parser: NSObject {
 	Parse thing object in JSON.
 	This method dispatches element of JSON to eithr methods to extract classes derived from Thing class.
 	*/
-    class func parseThing(json:JSONDictionary) -> Any? {
+    class func parseThing(json:JSONDictionary) -> Thing? {
         if let data = json["data"] as? JSONDictionary, kind = json["kind"] as? String {
             switch(kind) {
             case "t1":
                 // comment
-                return Comment(data:data)
+                return Thing(Comment(data:data))
             case "t2":
                 // account
-                return Account(data:data)
+                return Thing(Account(data:data))
             case "t3":
                 // link
-                return Link(data:data)
+                return Thing(Link(data:data))
             case "t4":
 				// mesasge
-                return Message(data:data)
+                return Thing(Message(data:data))
             case "t5":
                 // subreddit
-                return Subreddit(data:data)
+                return Thing(Subreddit(data:data))
 			case "more":
-                return More(data:data)
+                return Thing(More(data:data))
             case "LabeledMulti":
-                return Multireddit(json: data)
+                return Thing(Multireddit(json: data))
             case "LabeledMultiDescription":
-                return MultiredditDescription(json: data)
+                return Thing(MultiredditDescription(json: data))
             default:
                 break
             }
@@ -51,20 +51,15 @@ class Parser: NSObject {
 	/**
 	Parse list object in JSON
 	*/
-    class func parseListing(json:JSONDictionary) -> Listing {
+    class func parseListing(json:JSONDictionary) -> Thing {
         var listing = Listing()
         if let data = json["data"] as? JSONDictionary {
             if let children = data["children"] as? JSONArray {
                 for child in children {
                     if let child = child as? JSONDictionary {
-                        let obj:Any? = parseJSON(child)
+                        let obj:Thing? = parseJSON(child)
                         if let obj = obj {
-                            if let more:More = obj as? More {
-                                listing.more = more
-                            }
-                            else {
-                                listing.children.append(obj)
-                            }
+                            listing.children.append(obj)
                         }
                     }
                 }
@@ -80,26 +75,26 @@ class Parser: NSObject {
                 }
             }
         }
-        return listing
+        return Thing(listing)
     }
 	
 	/**
 	Parse JSON of the style which is Thing.
 	*/
-    class func parseJSON(json:JSON) -> Any? {
+    class func parseJSON(json:JSON) -> Thing? {
         // array
         // json->[AnyObject]
         if let array = json as? JSONArray {
-            var output:[Any] = []
+            var output:[Thing] = []
             for element in array {
                 if let element = element as? JSONDictionary {
-                    let obj:Any? = self.parseJSON(element)
-                    if let obj:Any = obj {
+                    let obj:Thing? = self.parseJSON(element)
+                    if let obj = obj {
                         output.append(obj)
                     }
                 }
             }
-            return output;
+            return Thing(output);
         }
 		// dictionary
 		// json->JSONDictionary
--- reddift/Utility/Result.swift
+++ reddift/Utility/Result.swift
@@ -8,6 +8,131 @@
 
 import Foundation
 
+public enum Thing {
+    case CommentType(Comment)
+    case LinkType(Link)
+    case MessageType(Message)
+    case MoreType(More)
+    case SubredditType(Subreddit)
+    case ListingType(Listing)
+    case MultiredditType(Multireddit)
+    case MultiredditDescriptionType(MultiredditDescription)
+    case AccountType(Account)
+    case ArrayType([Thing])
+    
+    public init(_ comment:Comment) {
+        self = .CommentType(comment)
+    }
+    public init(_ link:Link) {
+        self = .LinkType(link)
+    }
+    public init(_ message:Message) {
+        self = .MessageType(message)
+    }
+    public init(_ more:More) {
+        self = .MoreType(more)
+    }
+    public init(_ subreddit:Subreddit) {
+        self = .SubredditType(subreddit)
+    }
+    public init(_ listing:Listing) {
+        self = .ListingType(listing)
+    }
+    public init(_ multireddit:Multireddit) {
+        self = .MultiredditType(multireddit)
+    }
+    public init(_ multiredditdescription:MultiredditDescription) {
+        self = .MultiredditDescriptionType(multiredditdescription)
+    }
+    public init(_ account:Account) {
+        self = .AccountType(account)
+    }
+    public init(_ array:[Thing]) {
+        self = .ArrayType(array)
+    }
+    
+    public var comment:Comment? {
+        switch self {
+        case .CommentType(let comment):
+            return comment
+        default:
+            return nil
+        }
+    }
+    public var link:Link? {
+        switch self {
+        case .LinkType(let link):
+            return link
+        default:
+            return nil
+        }
+    }
+    public var message:Message? {
+        switch self {
+        case .MessageType(let message):
+            return message
+        default:
+            return nil
+        }
+    }
+    public var more:More? {
+        switch self {
+        case .MoreType(let more):
+            return more
+        default:
+            return nil
+        }
+    }
+    public var subreddit:Subreddit? {
+        switch self {
+        case .SubredditType(let subreddit):
+            return subreddit
+        default:
+            return nil
+        }
+    }
+    public var listing:Listing? {
+        switch self {
+        case .ListingType(let listing):
+            return listing
+        default:
+            return nil
+        }
+    }
+    public var multireddit:Multireddit? {
+        switch self {
+        case .MultiredditType(let multireddit):
+            return multireddit
+        default:
+            return nil
+        }
+    }
+    public var multiredditdescription:MultiredditDescription? {
+        switch self {
+        case .MultiredditDescriptionType(let multiredditdescription):
+            return multiredditdescription
+        default:
+            return nil
+        }
+    }
+    public var account:Account? {
+        switch self {
+        case .AccountType(let account):
+            return account
+        default:
+            return nil
+        }
+    }
+    public var array:[Thing]? {
+        switch self {
+        case .ArrayType(let array):
+            return array
+        default:
+            return nil
+        }
+    }
+}
+
 public final class Box<A> {
     public let value: A
     public init(_ value: A) {
