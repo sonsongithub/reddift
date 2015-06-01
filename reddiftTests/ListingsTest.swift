d8cf47abc96b7935103e33d95684069de960fe9b
sonson <yoshida.yuichi@gmail.com>
2015-06-02 01:16:16 +0900

    Adding a new test code...

--- reddiftTests/ListingsTest.swift
+++ reddiftTests/ListingsTest.swift
@@ -12,5 +12,33 @@ import Quick
 class ListingsTest: SessionTestSpec {
     
     override func spec() {
+        
+        describe("Test to download links.") {
+            it("") {
+//                let subreddit = Subreddit(id: "a")
+//                session?.getList(Paginator(), subreddit:subreddit, sort:sortTypes[seg.selectedSegmentIndex], timeFilterWithin:.All, completion: { (result) in
+//                    switch result {
+//                    case let .Failure:
+//                        println(result.error)
+//                    case let .Success:
+//                        println(result.value)
+//                        if let listing = result.value as? Listing {
+//                            for obj in listing.children {
+//                                if let link = obj as? Link {
+//                                    self.links.append(link)
+//                                }
+//                            }
+//                            self.paginator = listing.paginator
+//                        }
+//                        self.updateStrings()
+//                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                            self.tableView.reloadData()
+//                            self.loading = false
+//                        })
+//                    }
+//                })
+            }
+        }
+        
     }
 }
