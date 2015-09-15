e69b998fdf88cb56f0e863fee2b02f66a4fa3fbb
sonson <yoshida.yuichi@gmail.com>
2015-09-16 01:41:18 +0900

    Revert "Updated for cocoapods."
    This reverts commit 0edce9499e610ecba4431aa589ade5cf1db0eceb.

--- reddift/OAuth/OAuth2Authorizer.swift
+++ reddift/OAuth/OAuth2Authorizer.swift
@@ -67,18 +67,8 @@ public class OAuth2Authorizer {
         let currentState = self.state
         self.state = ""
         if (url.scheme == Config.sharedInstance.redirectURIScheme) {
-            if #available(OSX 10.10, *) {
-                if let temp = NSURLComponents(URL: url, resolvingAgainstBaseURL: true)?.dictionary() {
-                    parameters = temp
-                }
-            }
-            else {
-                url.query?.componentsSeparatedByString("&").forEach({ (aQuery) -> () in
-                    let keyAndValue = aQuery.componentsSeparatedByString("=")
-                    if keyAndValue.count == 2 {
-                        parameters[keyAndValue[0]] = keyAndValue[1]
-                    }
-                })
+            if let temp = NSURLComponents(URL: url, resolvingAgainstBaseURL: true)?.dictionary() {
+                parameters = temp
             }
         }
         if let code = parameters["code"], state = parameters["state"] {
