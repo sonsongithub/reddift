a75b5df6f7c1a6a18012b9f52e5989f11f901c55
sonson <yoshida.yuichi@gmail.com>
2015-09-16 08:05:21 +0900

    Replaced CFURLCreateStringByAddingPercentEscapes with stringByAddingPercentEncodingWithAllowedCharacters.

--- reddift/Model/Link.swift
+++ reddift/Model/Link.swift
@@ -14,7 +14,7 @@ extension String {
     */
     private func stringByAddingPercentEscapesUsingUTF8() -> String {
         let raw: NSString = self
-        let str = CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,raw,"","",CFStringConvertNSStringEncodingToEncoding(NSUTF8StringEncoding))
+        let str = raw.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet(charactersInString:"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$&'()*+,/:;=?@[]")) ?? raw
         return str as String
     }
 }
