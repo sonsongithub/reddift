b5d5c78277f67ccca178b9a4e256bbfe8be3c9e4
sonson <yoshida.yuichi@gmail.com>
2015-10-18 07:13:40 +0900

    Removed disused codes.

--- reddiftTests/ParseCommentMarkdownTest.swift
+++ reddiftTests/ParseCommentMarkdownTest.swift
@@ -27,14 +27,6 @@ class ParseCommentMarkdownTest: XCTestCase {
         
         /// Start to test
         self.measureBlock {
-            if let array = self.json as? [AnyObject] {
-                array.forEach({ (testData) -> () in
-                    if let dict = testData as? [String:AnyObject],
-                        let source = dict["source"] as? String {
-                            let _:NSAttributedString = source.markdown2attributedStringWithFontSize(14, superscriptFontSize: 10)
-                    }
-                })
-            }
         }
     }
     
@@ -43,51 +35,8 @@ class ParseCommentMarkdownTest: XCTestCase {
         print("Test whether Simple Markdown Parser can parse reddit's makrdown format.")
         print("test data : comment_parse_data.json")
         
-        /// Mapping test data's type to NSAttributedString's attribute name.
-        let typeDict:[String:String] = [
-            "link": NSLinkAttributeName,
-            "bold": NSFontAttributeName,
-            "italic": NSFontAttributeName,
-            "superscript": NSBaselineOffsetAttributeName,
-            "strike": NSStrikethroughStyleAttributeName
-        ]
-        
         /// Start to test
         if let array = self.json as? [AnyObject] {
-            array.forEach({ (testData) -> () in
-                print("----------------------------------------")
-                if let dict = testData as? [String:AnyObject],
-                    let body = dict["body"] as? String,
-                    let source = dict["source"] as? String,
-                    let attr = dict["attr"] as? [AnyObject] {
-                        /// Parse
-                        let attributedString:NSAttributedString = source.markdown2attributedStringWithFontSize(14, superscriptFontSize: 10)
-                        print("------->Input markdown")
-                        print(source)
-                        print("------->Ground truth")
-                        print(body)
-                        print("------->Parser's result")
-                        print(attributedString.string)
-                        
-                        /// Test string
-                        XCTAssert(body == attributedString.string)
-                        
-                        /// Test all attributes that must be attached NSAttributedString.
-                        attr.forEach({(item) -> () in
-                            var check = false
-                            if let item = item as? [String:AnyObject], let type = item["type"] as? String, let name = typeDict[type] {
-                                if let location = item["location"] as? Int, let length = item["length"] as? Int {
-                                    print(location)
-                                    print(length)
-                                    attributedString.enumerateAttribute(name, inRange: NSMakeRange(location - 1, length), options: NSAttributedStringEnumerationOptions(), usingBlock: { (value:AnyObject?, range:NSRange, stop:UnsafeMutablePointer<ObjCBool>) -> Void in
-                                        check = (value != nil && (location - 1) == range.location && length == range.length)
-                                    })
-                                }
-                            }
-                            XCTAssert(check == true)
-                        })
-                }
-            })
         }
     }
     
