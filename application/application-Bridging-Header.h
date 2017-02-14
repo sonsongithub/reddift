6eb7398c4f4b8a986ffbf6c66d787a35e5d045f6
sonson <yoshida.yuichi@gmail.com>
2017-02-15 02:09:41 +0900

    Added HTMLSpecialCharacters as submodule. Added Cartfile for application. Revised source code for using framework.

--- application/application-Bridging-Header.h
+++ application/application-Bridging-Header.h
@@ -5,12 +5,7 @@
 #ifndef reddift_objc_header_h
 #define reddift_objc_header_h
 
-#import "UZTextView.h"
 #import "ReddiftTextView.h"
 #import <CommonCrypto/CommonCrypto.h>
-#import "GTMDefines.h"
-#import "GTMNSString+HTML.h"
-#import "FLAnimatedImageView.h"
-#import "FLAnimatedImage.h"
 
 #endif
