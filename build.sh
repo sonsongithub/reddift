57063230cfe190b7854300090f2472bdf071e953
sonson <yoshida.yuichi@gmail.com>
2018-05-01 11:51:46 +0900

    Keychain...?

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
 echo "Create keychain"
-security create-keychain -p travis build.keychain
-security default-keychain -s build.keychain
-security unlock-keychain -p travis build.keychain
-security set-keychain-settings -t 3600 -l ~/Library/Keychains/build.keychain
+# Create the keychain with a password
+security create-keychain -p travis ios-build.keychain
 
-# security import ./Lib/Certificates/apple.cer -k ~/Library/Keychains/build.keychain -T /usr/bin/codesign
+# Make the custom keychain default, so xcodebuild will use it for signing
+security default-keychain -s ios-build.keychain
+
+# Unlock the keychain
+security unlock-keychain -p travis ios-build.keychain
+
+# Add certificates to keychain and allow codesign to access them
+security import ./Provisioning/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+
+security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
 
 echo "Build start"
 xcodebuild test -project reddift.xcodeproj -scheme reddift-iOS -configuration Debug -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 8"
