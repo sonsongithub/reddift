e2f27c4d2e00e59172798c885a44209265a714a8
sonson <yoshida.yuichi@gmail.com>
2018-05-01 12:21:14 +0900

    I don't know how to fix it...

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ security default-keychain -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
 
 # Add certificates to keychain and allow codesign to access them
-security import ./Provisioning/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+# security import ./Provisioning/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 
 security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
 
