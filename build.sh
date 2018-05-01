c3b37c023edcc0bdf0fac836c31254263c46425a
sonson <yoshida.yuichi@gmail.com>
2018-05-01 11:37:03 +0900

    Keychain...?

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
+echo "Create keychain"
+security create-keychain -p travis build.keychain
+security default-keychain -s build.keychain
+security unlock-keychain -p travis build.keychain
+security set-keychain-settings -t 3600 -l ~/Library/Keychains/build.keychain
+
+# security import ./Lib/Certificates/apple.cer -k ~/Library/Keychains/build.keychain -T /usr/bin/codesign
+
+echo "Build start"
 xcodebuild test -project reddift.xcodeproj -scheme reddift-iOS -configuration Debug -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 8"
